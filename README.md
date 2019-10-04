0007fde7a33aefe4e8ac54ff240914c9d8ebb2a3
Christian Eder <ederc@mathematik.uni-kl.de>
2019-10-04 11:16:08 +0200

    Add autogenerated files to build libgb

---
+++ README.md
@@ -0,0 +1,9 @@
+# GBBuilder
+
+[![Build Status](https://travis-ci.org/ederc/GBBuilder.svg?branch=master)](https://travis-ci.org/ederc/GBBuilder)
+
+This repository builds binary artifacts for the libgb project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/ederc/GBBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libgb"
+version = v"0.13.0-dev"
+
+# Collection of sources required to build libgb
+sources = [
+    "https://github.com/ederc/gb.git" =>
+    "8bb6c28d30650d3e1bb1adf7016e48372164e520",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd gb/
+./autogen.sh 
+echo $CFLAGS
+./configure -h
+CFLAGS="-march=core2" ./configure --enable-shared --disable-static --with-gmp=$prefix --prefix=$prefix --host=$target
+grep "core2" config.log
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libgb--no-undefined", Symbol(""))
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/GMP-v6.1.2-1/build_GMP.v6.1.2.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
