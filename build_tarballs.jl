7c8a1b8b9fe923b7afa2e066498dfa8278c9be23
ederc <ederc@mathematik.uni-kl.de>
2019-10-04 10:27:40 +0000

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -17,7 +17,7 @@ script = raw"""
 cd $WORKSPACE/srcdir
 cd gb/
 ./autogen.sh 
-CFLAGS="-march=core2" ./configure --enable-shared --disable-static --with-gmp=$prefix --prefix=$prefix --host=$target
+./configure --enable-shared --disable-static --with-gmp=$prefix --prefix=$prefix --host=$target
 make
 make install
 
